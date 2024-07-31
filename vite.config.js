f5798a0a6ca93460b718fea7b951a17d514a694d
OneSuchDan <bryan_macias007@outlook.com>
2024-07-30 23:05:50 -0500

    Fix vite.config to png extension

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "https://onesuchdan.github.io/OneSuchDaniel.github.io",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve("./src/assets/icon_person.png", "src/assets"),
+    },
+  },
 })
