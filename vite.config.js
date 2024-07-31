7fb269c75e833da54711945a9ee7d83d01b5cb42
OneSuchDan <bryan_macias007@outlook.com>
2024-07-30 23:19:49 -0500

    Fix bug source

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "https://onesuchdan.github.io/OneSuchDaniel.github.io",
-  plugins: [react()],
+  plugins: [react()
+    ,viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
